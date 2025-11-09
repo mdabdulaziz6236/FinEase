5c471c6401c5ee5ed78c2966e270b55e2e9212f2
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-11-09 19:30:08 +0600

    Set Up react-router and added  Toaster

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import Navbar from "../Components/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../Components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const MainLayout = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return <div>
+
+    <Navbar></Navbar>
+    <div>
+        <Outlet></Outlet>
+    </div>
+    <Footer></Footer>
+    <Toaster position="top-right"></Toaster>
+  </div>;
 };
 
 export default MainLayout;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/Home/Home";
+
+export const router = createBrowserRouter([{
+    path:'/',
+    element:<MainLayout></MainLayout>,
+    children:[
+        {
+            index:true,
+            Component:Home,
+        }
+    ]
+}])
