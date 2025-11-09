cf9f2ad4e312bbc5ad0d66febb3c9b36ce27e6b6
mdabdulaziz6236 <md.abdulaziz6236@gmail.com>
2025-11-09 19:15:50 +0600

    In the name of Allah, Projects Started with react-vite and  installed some package

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+      <h1>Bismillahir Rahmanir Rahim.</h1>
+      <h3>Salli Alal Habib wa barik wa alihi wa sallama.</h3>
+    </>
+  )
+}
+
+export default App
